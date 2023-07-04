39f9c23baecfc198bab769ce6e5325419af16063
AndriiKot <a_kocyuba@ukr.net>
2023-07-04 21:26:35 +0100

    delete grid-templete-areas

--- COLORS/javascript.js
+++ COLORS/javascript.js
@@ -16,14 +16,13 @@ function callsubmit(event){
     event.preventDefault();
     deleteDivWrapperToBody();
     addDivWrapperToBody();
-    setGridTemplateAreas();
 }
 
 function getAsisXandY(){
     const x = Number(gridAxisX.value) || 0;
     const y = Number(gridAxisY.value) || 0;
-    return [x,y]
-}
+    return [x,y];
+};
 
 function getNumberOfElements() {
     const [x,y] = getAsisXandY();
@@ -32,7 +31,7 @@ function getNumberOfElements() {
 
 function getDivContainerWrapper(){
    return document.querySelector('.wrapper');
-}
+};
 
 function getNodeListDivColors(){
     const nodeListGridElements = document.querySelectorAll("[class^='color-']");
@@ -87,67 +86,6 @@ function randomInteger(min,max){
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
 };
-
-function createNameGridAreaElements(){
-    const nodeListDivColors = getNodeListDivColors();
-    const arrayGridArea = []
-    for(let i = 0; i < nodeListDivColors.length; i++){
-       const value = nodeListDivColors[i].className;
-       nodeListDivColors[i].style.gridArea = value;
-       arrayGridArea.push(value)
-    };
-    return arrayGridArea;
-};
-
-function createGridViewString(){
-    const arr = createNameGridAreaElements();
-    if(arr.length === 0) { return };
-    const [for1,for2] = getAsisXandY();
-    let fullString =  ``;
-    let elementToArray = 0;
-    for(let i = 0; i < for1; i++){
-        let string = ``;
-        for(let i = 0; i < for2; i++){
-            string += arr[elementToArray] + ' ';
-            elementToArray += 1;
-        };
-        fullString += (string + "\n");
-    };
-    return fullString;
-};
-
-function setGridTemplateAreas() {
-    const wrapper = getDivContainerWrapper();
-    if(wrapper == false){ return };
-    wrapper.style.gridTemplateAreas = `
-    ${createGridViewString()}
-    `
-}
-
-
-// console.log(divColors)
-
-
-// let cloneGridContainers = [...gridContainers];
-
-// for(let i = 0; i < divColors.length; i++){
-//     const random = randomInteger(0,cloneGridContainers.length - 1);
-//     let index = cloneGridContainers[random];
-//     cloneGridContainers = cloneGridContainers.filter(function(f){return f !== index});
-
-    
-//      newGridContainers = newGridContainers.with(index,divColors[i]);
-// }
-
-
-
-// body.style.gridTemplateAreas = `
-// "color-1 color-1"
-// "color-3 color-3"
-// "color-2 color-4"
-// "color-5 color-5"
-// `
-
 
 
 
