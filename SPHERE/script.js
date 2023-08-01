31ab6a23fb57da83b1bebf139074f1a034790b04
AndriiKot <a_kocyuba@ukr.net>
2023-08-01 10:33:26 +0100

    corected code style

--- SPHERE/script.js
+++ SPHERE/script.js
@@ -2,7 +2,7 @@
 
 const body = document.body;
 const buttonStart = document.querySelector('.start');
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 
 
 buttonStart.addEventListener('click',createGridElements);
@@ -17,10 +17,13 @@ function createGridElement(number = 0) {
 };
 
 function createGridElements(numberOfElements = 0) {
+
  const [x,y] = [Number(gridAxisX.value),Number(gridAxisY.value)];
  numberOfElements = x * y;
+
  const sizeConrainer = window.innerWidth / Number(gridAxisX.value);
  const repeatGridView = Math.floor(sizeConrainer - 20);
+
  (Object.assign(document.documentElement, {
   style: `
     --size-container: ${sizeConrainer}px;
@@ -31,7 +34,7 @@ function createGridElements(numberOfElements = 0) {
 
  let fullElements = ``;  
  for(let i = 1; i <= numberOfElements; i++){
-  const element = createGridElement(i)
+  const element = createGridElement(i);
 
   fullElements += element;
  };
@@ -40,7 +43,7 @@ function createGridElements(numberOfElements = 0) {
 };
 
 function addAmination() {
-const elements = document.querySelectorAll('.box-animation')
+const elements = document.querySelectorAll('.box-animation');
 const deg = 20;
 const color_start = '#00cc99';
 const color_end  = 'transparent';
@@ -49,38 +52,38 @@ const max_procent_width_line = 100;
 const step_procent_width_line = 30;
 let str = '';
 
-let i = 0
+let i = 0;
 
 for (;procent_width_line <= max_procent_width_line;i++){
- const a = `${color_start} ${procent_width_line}%`
- const b = `${color_end} ${procent_width_line}%`
+ const a = `${color_start} ${procent_width_line}%`;
+ const b = `${color_end} ${procent_width_line}%`;
  const compon_str = (i % 2 == 0) ? a +','+ b : b + ',' + a;
   
-  str += compon_str +','
+  str += compon_str +',';
   procent_width_line += step_procent_width_line;
-}
+};
 
-let final_str = `linear-gradient(${deg}deg,${str})`
+let final_str = `linear-gradient(${deg}deg,${str})`;
 
 let nIntervId;
     
 function animation_rotate_interval(element) {
   nIntervId = setInterval(() =>{calc_animation_rotate(element)},100);
-}
+};
 
 function calc_animation_rotate(element){
-  final_str = `linear-gradient(${deg}deg,${str}`
-  final_str = final_str.slice(0,final_str.length-1)+')'
+  final_str = `linear-gradient(${deg}deg,${str}`;
+  final_str = final_str.slice(0,final_str.length-1)+')';
   element.style.background = final_str;
-}
+};
 
 
 
   for(let i = 0; elements.length > i; i++){
     const el = elements[i];
     animation_rotate_interval(el);
-  }
-}
+  };
+};
 
 
   
