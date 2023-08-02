d4065ae6e468d9f2f40e62ccc7de963782c57ede
AndriiKot <a_kocyuba@ukr.net>
2023-08-02 09:47:12 +0100

    create function addAnimation

--- SPHERE/script.js
+++ SPHERE/script.js
@@ -3,6 +3,8 @@
 const body = document.body;
 const buttonStart = document.querySelector('.start');
 const container = document.querySelector('.container');
+let nIntervId;
+
 
 
 buttonStart.addEventListener('click',createGridElements);
@@ -44,7 +46,7 @@ function createGridElements(numberOfElements = 0) {
  for(let i=0; elements.length > i; i++){
   console.log(elements[i])
  }
- addAmination();
+//  addAmination();
 };
 
 function randomInteger(min,max){
@@ -64,48 +66,49 @@ function getRandomColros(){
     return [r,g,b];
 };
 
-
-function addAmination() {
-const elements = document.querySelectorAll('.box-animation');
-
 function color() {
   const [r,g,b] = getRandomColros();
   return `rgb(${r},${g},${b})`
 };
 
-const deg = 20;
-const color_start = color();
-const color_end  = 'transparent';
-let procent_width_line = 0;
-const max_procent_width_line = 100;
-const step_procent_width_line = 30;
-let str = '';
-
-
-let i = 0;
-
-for (;procent_width_line <= max_procent_width_line;i++){
- const a = `${color_start} ${procent_width_line}%`;
- const b = `${color_end} ${procent_width_line}%`;
- const compon_str = (i % 2 == 0) ? a +','+ b : b + ',' + a;
-  
-  str += compon_str +',';
-  procent_width_line += step_procent_width_line;
-};
-
-let final_str = `linear-gradient(${deg}deg,${str})`;
-
-let nIntervId;
-    
 function animation_rotate_interval(element) {
   nIntervId = setInterval(() =>{calc_animation_rotate(element)},100);
 };
 
-function calc_animation_rotate(element){
+function calc_animation_rotate(element,deg,str,final_str){
   final_str = `linear-gradient(${deg}deg,${str}`;
   final_str = final_str.slice(0,final_str.length-1)+')';
   element.style.background = final_str;
 };
+
+function setDEG(int = 20){
+  return int;
+};
+
+function createLineAnimation(){
+
+}
+
+
+
+function addAmination(color_end ='transparent',max_procent_width_line = 100,step_procent_width_line = 30) {
+  const color_start = color();
+  let procent_width_line = 0;
+  let str = '';
+  
+  
+  let i = 0;
+  
+  for (;procent_width_line <= max_procent_width_line;i++){
+   const a = `${color_start} ${procent_width_line}%`;
+   const b = `${color_end} ${procent_width_line}%`;
+   const compon_str = (i % 2 == 0) ? a +','+ b : b + ',' + a;
+    
+    str += compon_str +',';
+    procent_width_line += step_procent_width_line;
+};
+
+}
 
 
 
