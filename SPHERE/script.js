0f7d04bd1c285e54ece67897ccf0da0653641b72
AndriiKot <a_kocyuba@ukr.net>
2023-08-02 08:55:33 +0100

    add function color

--- SPHERE/script.js
+++ SPHERE/script.js
@@ -63,21 +63,22 @@ function getRandomColros(){
 function addAmination() {
 const elements = document.querySelectorAll('.box-animation');
 const deg = 20;
-const [r,g,b] = getRandomColros();
-// console.log(r,g,b)
-const color_start = '#00cc99';
-const color_demo = `rgb(${r},${g},${b})`
-// console.log(color_demo)
+function color() {
+  const [r,g,b] = getRandomColros();
+  return `rgb(${r},${g},${b})`
+}
 const color_end  = 'transparent';
 let procent_width_line = 0;
 const max_procent_width_line = 100;
 const step_procent_width_line = 30;
 let str = '';
+const color_start = color();
+
 
 let i = 0;
 
 for (;procent_width_line <= max_procent_width_line;i++){
- const a = `${color_demo} ${procent_width_line}%`;
+ const a = `${color_start} ${procent_width_line}%`;
  const b = `${color_end} ${procent_width_line}%`;
  const compon_str = (i % 2 == 0) ? a +','+ b : b + ',' + a;
   
