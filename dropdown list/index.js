e5a7d5e1a4fd35459f92f4ddacee1fc91661e730
AndriiKot <a_kocyuba@ukr.net>
2023-12-13 23:54:16 +0100

    add styles and htlm elements

--- dropdown list/index.js
+++ dropdown list/index.js
@@ -1,1 +1,7 @@
-﻿
+﻿const selectButton = document.querySelector('.select-btn')
+const dropDown = document.querySelector('.list-items')
+
+console.log(selectButton)
+console.log(dropDown)
+
+selectButton.addEventListener('click',() => { console.log("HI!!") });
