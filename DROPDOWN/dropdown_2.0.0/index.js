038c6f738156d92b31521fa21c193047fb8fbef1
AndriiKot <a_kocyuba@ukr.net>
2023-12-17 11:27:26 +0100

    dropdown 1.0.0 and 2.0.0

---
+++ DROPDOWN/dropdown_2.0.0/index.js
@@ -0,0 +1,18 @@
+﻿const selectButton = document.querySelector(".select-btn");
+const dropDown = document.querySelector(".list-items");
+const dropDownClassOpen = "open";
+
+const handleSelectButtonClick = (element, addClass) => {
+  const el = element;
+  const c = addClass;
+  el.classList.toggle(c);
+};
+
+selectButton.addEventListener(
+  "click",
+  handleSelectButtonClick.bind(
+    handleSelectButtonClick,
+    dropDown,
+    dropDownClassOpen
+  )
+);
