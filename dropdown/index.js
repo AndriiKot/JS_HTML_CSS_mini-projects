368ff44b725a184b878f57c8c376f830887b15eb
AndriiKot <a_kocyuba@ukr.net>
2023-12-17 02:34:01 +0100

    continuation tutorials 'js book'

---
+++ dropdown/index.js
@@ -0,0 +1,16 @@
+﻿const selectButton = document.querySelector('.select-btn')
+const dropDown = document.querySelector('.list-items')
+const dropDownClassOpen = 'open';
+
+const handleSelectButtonClick = (element,addClass) => {
+    const fn = (_event,el = element, c = addClass) => {
+        el.classList.toggle(c);
+    }
+    return fn;
+}
+
+selectButton.addEventListener('click', handleSelectButtonClick(dropDown,dropDownClassOpen));
+
+
+   
+
