525c8bf0ba01102f51b719b604f45da4e1622552
AndriiKot <a_kocyuba@ukr.net>
2023-12-19 16:27:50 +0100

    ending demo version dropdown_1.0.0

--- DROPDOWN/dropdown_1.0.0/index.js
+++ DROPDOWN/dropdown_1.0.0/index.js
@@ -20,9 +20,20 @@ const toggleCheckbox = (e) => {
   const item_input = e.currentTarget.querySelector("input");
   if (e.target.nodeName === "INPUT") {
     item_input.checked = !item_input.checked;
-  };
+  }
   if (item_input) {
     item_input.checked = !item_input.checked;
+  }
+  const nodeListItems = document.querySelectorAll("input");
+  let count = 0;
+  nodeListItems.forEach((item) => {
+    if (item.checked) {
+      count++;
+    }
+  });
+  const selectButtonTextContent = document.querySelector(".text-content");
+  if (item_input) {
+    selectButtonTextContent.textContent = `Selected ${count}`;
   }
 };
 
