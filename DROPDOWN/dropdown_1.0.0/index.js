469238c8f577b9496b3bc925f2b3fa702455ccfd
AndriiKot <a_kocyuba@ukr.net>
2023-12-19 19:21:48 +0100

    test function

--- DROPDOWN/dropdown_1.0.0/index.js
+++ DROPDOWN/dropdown_1.0.0/index.js
@@ -1,14 +1,17 @@
 ﻿const selectButton = document.querySelector(".select-btn");
 const dropDown = document.querySelector(".list-items");
 const liNodeList = document.querySelectorAll(".item");
-const defaultButtonTextContent = document.querySelector('.text-content').innerText;
-const dropDownClassOpen = "active";
+const defaultButtonTextContent =
+  document.querySelector(".text-content").innerText;
+const selectButtonActive = "active";
+const dropDownExists = "exists";
 
 console.log(defaultButtonTextContent);
 
 const handleSelectButtonClick = (element, addClass) => {
   const fn = (event, el = element, c = addClass) => {
     el.classList.toggle(c);
+    el.classList.toggle(dropDownExists);
     event.currentTarget.classList.toggle(c);
   };
   return fn;
@@ -16,7 +19,7 @@ const handleSelectButtonClick = (element, addClass) => {
 
 selectButton.addEventListener(
   "click",
-  handleSelectButtonClick(dropDown, dropDownClassOpen)
+  handleSelectButtonClick(dropDown, selectButtonActive)
 );
 
 const toggleCheckbox = (e) => {
@@ -36,7 +39,7 @@ const toggleCheckbox = (e) => {
   });
   const selectButtonTextContent = document.querySelector(".text-content");
   if (count) {
-    console.log(count)
+    console.log(count);
     selectButtonTextContent.textContent = `Selected ${count}`;
   } else {
     selectButtonTextContent.textContent = `${defaultButtonTextContent}`;
