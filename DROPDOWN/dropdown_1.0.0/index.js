dfc12778dd7ccf2b1d9604a7f86d3bf57179228d
AndriiKot <a_kocyuba@ukr.net>
2023-12-20 12:24:30 +0100

    finished v 1.0.0

--- DROPDOWN/dropdown_1.0.0/index.js
+++ DROPDOWN/dropdown_1.0.0/index.js
@@ -1,54 +1,55 @@
 ﻿const dropDownMainWrapper = document.querySelector(".wrapper");
 
 const selectButton = dropDownMainWrapper.querySelector(".select-btn");
-const defaultButtonTextContent = selectButton.querySelector(".text-content").innerText;
+const defaultButtonTextContent =
+  selectButton.querySelector(".text-content").innerText;
+const newButtonTextContent = "Selected ";
 const selectButtonActiveClassName = "active";
 
 const dropDown = dropDownMainWrapper.querySelector(".list-items");
 const liNodeList = dropDown.querySelectorAll(".item");
 const dropDownExistsClassName = "exists";
 
-
-const handleSelectButtonClick = (element,className) => {
+const handleSelectButtonClick = (element, className) => {
   element.classList.toggle(className);
 };
 
-selectButton.addEventListener("click", handleSelectButtonClick.bind(this, selectButton, selectButtonActiveClassName));
-selectButton.addEventListener("click", handleSelectButtonClick.bind(this, dropDown, dropDownExistsClassName));
+selectButton.addEventListener(
+  "click",
+  handleSelectButtonClick.bind(this, selectButton, selectButtonActiveClassName)
+);
 
+selectButton.addEventListener(
+  "click",
+  handleSelectButtonClick.bind(this, dropDown, dropDownExistsClassName)
+);
 
 const toggleCheckbox = (e) => {
-  if (e.target.tagName === 'INPUT') return;
+  if (e.target.type === "checkbox") return;
 
-  const item_input = e.currentTarget.querySelector("input");
-  item_input.checked = (item_input.checked) ? false : true
+  const checkbox = e.currentTarget.querySelector("input");
+  checkbox.checked = checkbox.checked ? false : true;
 };
 
-const newTextContentSelectButton = () => {
-  console.log('TEXT content')
-
+const newTextContentSelectButton = (element, oldValue, newValue) => {
   const nodeListItems = document.querySelectorAll("input");
   let count = 0;
   nodeListItems.forEach((item) => {
-    if (item.checked) {
-      count++;
-    }
+    if (item.checked) count++;
   });
-  const selectButtonTextContent = document.querySelector(".text-content");
-  if (count) {
-    console.log(count);
-    selectButtonTextContent.textContent = `Selected ${count}`;
-  } else {
-    selectButtonTextContent.textContent = `${defaultButtonTextContent}`;
-  }
+  element.textContent = count ? newValue + String(count) : oldValue;
+};
 
-}
-
+const selectButtonTextContent = document.querySelector(".text-content");
 
 liNodeList.forEach((item) => {
-  item.addEventListener('click',toggleCheckbox)
-  item.addEventListener('click', newTextContentSelectButton)
-})
-
-
-
+  item.addEventListener("click", toggleCheckbox);
+  item.addEventListener(
+    "click",
+    newTextContentSelectButton.bind(
+      this,
+      selectButtonTextContent,
+      defaultButtonTextContent,
+      newButtonTextContent    )
+  );
+});
