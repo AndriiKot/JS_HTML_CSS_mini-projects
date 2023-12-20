2084dc53ed528c0cd0cceb59f2ba8d3425635c6a
AndriiKot <a_kocyuba@ukr.net>
2023-12-20 21:57:47 +0100

    add event

--- LINKSANIMATION/js/index.js
+++ LINKSANIMATION/js/index.js
@@ -6,6 +6,10 @@ const highlight = document.createElement('span');
 highlight.classList.add("highlight");
 document.body.appendChild(highlight);
 
+const displayHighlight = () => {
+    console.log("Hi!!!")
+}
 
-
-
+links.forEach((link) => {
+    link.addEventListener('mouseover',displayHighlight);
+})
