009624fa9dddc2a2ec3ac41d9918e96cc9a0a8cb
AndriiKot <a_kocyuba@ukr.net>
2023-12-20 23:12:34 +0100

    test 2 link animation

--- LINKSANIMATION/js/index.js
+++ LINKSANIMATION/js/index.js
@@ -10,9 +10,17 @@ document.body.appendChild(highlight);
 function displayHighlight() {
     const linkCoords = this.getBoundingClientRect();
 
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX,
+    };
+
     highlight.style.width = `${linkCoords.width}px`;
     highlight.style.height = `${linkCoords.height}px`;
-    highlight.style.transform = `translate(${linkCoords.left}px,${linkCoords.top}px)`
+    highlight.style.transform = `translate(${coords.left}px,${coords.top}px)`;
+
 }
 
 links.forEach((link) => {
