a002112c402bfb069247ae8c863bd2462a032cb1
AndriiKot <a_kocyuba@ukr.net>
2023-12-22 18:50:06 +0100

    delete node_modules

---
+++ DROPDOWN/js/dropdown_1.0.1(plus webpack)/app.js
@@ -0,0 +1,44 @@
+﻿
+
+import {
+  handleClick,
+  toggleCheckbox,
+  newTextContent,
+} from './src/functions.js';
+
+const selectButton = document.querySelector('.select-btn');
+const defaultButtonTextContent =
+  selectButton.querySelector('.text-content').innerText;
+const newButtonTextContent = 'Selected ';
+const selectButtonActiveClassName = 'active';
+
+const dropDown = document.querySelector('.list-items');
+const liNodeList = dropDown.querySelectorAll('.item');
+const checkboxClassName = '.list-items__item__checkbox';
+const dropDownExistsClassName = 'exists';
+
+const selectButtonTextContent = document.querySelector('.text-content');
+
+selectButton.addEventListener(
+  'click',
+  handleClick.bind(this, selectButton, selectButtonActiveClassName)
+);
+
+selectButton.addEventListener(
+  'click',
+  handleClick.bind(this, dropDown, dropDownExistsClassName)
+);
+
+liNodeList.forEach((item) => {
+  item.addEventListener('click', toggleCheckbox);
+  item.addEventListener(
+    'click',
+    newTextContent.bind(
+      this,
+      selectButtonTextContent,
+      defaultButtonTextContent,
+      newButtonTextContent,
+      checkboxClassName
+    )
+  );
+});
---
+++ DROPDOWN/js/dropdown_1.0.1(plus webpack)/src/functions.js
@@ -0,0 +1,24 @@
+﻿
+
+const handleClick = (element, className) => {
+  element.classList.toggle(className);
+};
+
+const toggleCheckbox = (e) => {
+  if (e.target.type === 'checkbox') return;
+
+  const checkbox = e.currentTarget.querySelector('input[type=\'checkbox\']');
+  checkbox.checked = !checkbox.checked;
+};
+
+const newTextContent = (element, oldValue, newValue, selectorNodeList = '') => {
+  const nodeListItems = document.querySelectorAll(selectorNodeList);
+  let count = 0;
+  nodeListItems.forEach((item) => {
+    if (item.checked) count++;
+  });
+  element.textContent = count ? newValue + String(count) : oldValue;
+};
+
+
+export { handleClick, toggleCheckbox, newTextContent };
