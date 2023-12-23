6d4c46e943b4a88f771a25be43c7847bfb121e15
AndriiKot <a_kocyuba@ukr.net>
2023-12-23 22:48:03 +0100

    getArrayRandomBorderRadius

--- ANIMATION BUTTONS/script.js
+++ ANIMATION BUTTONS/script.js
@@ -1,14 +1,19 @@
 ﻿let crazy_button = document.querySelector('button')
 
-function randomInteger(min = 0, max = 100){
+function randomInteger0_100(){
+  const min = 0;
+  const max = 100;
   let rand = min + Math.random() * (max + 1 - min);
   return  Math.floor(rand);
 }
 
-function myCrazyButton(element, fn = randomInteger){ 
+function myCrazyButton(element, fn = randomInteger0_100){ 
   let el_1 = `${fn()}% ${fn()}% ${fn()}% ${fn()}%`;
   let el_2 = `${fn()}% ${fn()}% ${fn()}% ${fn()}%`;
    return element.style.borderRadius = el_1 + ' / ' +  el_2;
+}
+const randomBorderRadius = (countElements = 8,fn = randomInteger0_100) => { 
+  return [...new Array(countElements)].map(() => fn()); 
 }
 
 const handleClick = (element, className) => {
@@ -22,6 +27,7 @@ const animationStart = (element) => {
   }
   myCrazyButton(element)
   console.log(element.style.borderRadius)
+  console.log(randomBorderRadius());
 }
 
 
