f2d37073d287d41faed0a3ead29bc5a14f4b4608
AndriiKot <a_kocyuba@ukr.net>
2023-12-23 22:11:39 +0100

    add function crazyButton

--- ANIMATION BUTTONS/script.js
+++ ANIMATION BUTTONS/script.js
@@ -1,16 +1,14 @@
 ﻿let crazy_button = document.querySelector('button')
 
-function randomInteger(min,max){
-  min = 0;max = 100;
+function randomInteger(min = 0, max = 100){
   let rand = min + Math.random() * (max + 1 - min);
   return  Math.floor(rand);
 }
 
-function myCrazyButton(element){ 
-  let a = randomInteger;
-  let b = `${a()}% ${a()}% ${a()}% ${a()}%`;
-  let c = `${a()}% ${a()}% ${a()}% ${a()}%`;
-   return element.style.borderRadius = b + ' / ' +  c;
+function myCrazyButton(element, fn = randomInteger){ 
+  let el_1 = `${fn()}% ${fn()}% ${fn()}% ${fn()}%`;
+  let el_2 = `${fn()}% ${fn()}% ${fn()}% ${fn()}%`;
+   return element.style.borderRadius = el_1 + ' / ' +  el_2;
 }
 
 const handleClick = (element, className) => {
@@ -19,6 +17,7 @@ const handleClick = (element, className) => {
 
 const animationStart = (element) => {
   console.dir(element);
+  myCrazyButton(element)
 }
 
 
