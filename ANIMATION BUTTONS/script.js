4010ae56073ce912d11263f734598f0b0a71e40e
AndriiKot <a_kocyuba@ukr.net>
2023-12-24 00:05:54 +0100

    testing while

--- ANIMATION BUTTONS/script.js
+++ ANIMATION BUTTONS/script.js
@@ -1,62 +1,60 @@
-﻿let crazy_button = document.querySelector('button')
+﻿let crazy_button = document.querySelector("button");
 
-function randomInteger0_100(){
+function randomInteger0_100() {
   const min = 0;
   const max = 100;
   let rand = min + Math.random() * (max + 1 - min);
-  return  Math.floor(rand);
+  return Math.floor(rand);
 }
 
-function myCrazyButton(element, fn = randomInteger0_100){ 
+function myCrazyButton(element, fn = randomInteger0_100) {
   let el_1 = `${fn()}% ${fn()}% ${fn()}% ${fn()}%`;
   let el_2 = `${fn()}% ${fn()}% ${fn()}% ${fn()}%`;
-   return element.style.borderRadius = el_1 + ' / ' +  el_2;
+  return (element.style.borderRadius = el_1 + " / " + el_2);
 }
 
-const getArrayRandomBorderRadius = (countElements = 8,fn = randomInteger0_100) => { 
-  return [...new Array(countElements)].map(() => fn()); 
-}
+const getArrayRandomBorderRadius = (
+  countElements = 8,
+  fn = randomInteger0_100
+) => {
+  return [...new Array(countElements)].map(() => fn());
+};
 
 const getArrayDefaultBorderRadius = (countElements = 8, element = 100) => {
   return [...new Array(countElements)].map(() => element);
-} 
+};
 
-const comparison_of_values = (el_1,el_2) => {
+const comparison_of_values = (el_1, el_2) => {
   el_1 = getArrayDefaultBorderRadius();
   el_2 = getArrayRandomBorderRadius();
-  el_2 = [100,100,100,10,100,100,100,100]
-  console.log(el_1,el_2)
+  el_2 = [100, 100, 100, 2, 10, 2, 100, 100];
   let count = 0
-  for(let i = 0; i < el_1.length ; i++) {
-    if (el_1[i] === el_2[i]) {
-      console.log('el1 === el3')
-      count++
-      continue
-    } else {
-      console.log('el1 > or < el2')
-      el_1[i] > el_2[i] ? el_1[i] -= 1 : el_1[i] += 1
+  while (count < 8) {
+    count = 0;
+    for (let i = 0; i < el_1.length; i++) {
+      if (el_1[i] === el_2[i]) {
+        count++;
+        continue;
+      } else {
+        el_1[i] > el_2[i] ? (el_1[i] -= 1) : (el_1[i] += 1);
+      }
     }
+    
+    console.log(count);
   }
-  console.log(count)
-}
+};
 
 comparison_of_values();
 
 const animationStart = (element) => {
   if (!element.style.borderRadius) {
     element.style.borderRadius = `0% 0% 0% 0% / 0% 0% 0% 0%`;
-    console.log("Empty")
+    console.log("Empty");
   }
-  myCrazyButton(element)
-  console.log(element.style.borderRadius)
+  myCrazyButton(element);
+  console.log(element.style.borderRadius);
   console.log(getArrayRandomBorderRadius());
-  console.log(getArrayDefaultBorderRadius())
-}
+  console.log(getArrayDefaultBorderRadius());
+};
 
-
-crazy_button.addEventListener('click',animationStart.bind(this,crazy_button));
-
-
-
-
-
+crazy_button.addEventListener("click", animationStart.bind(this, crazy_button));
