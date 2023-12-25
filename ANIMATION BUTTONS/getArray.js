2ec33a8d9af8fdd06221e81e1ade65590d69a7b1
AndriiKot <a_kocyuba@ukr.net>
2023-12-25 14:24:38 +0100

    rename

---
+++ ANIMATION BUTTONS/getArray.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+const getArrayRandomBorderRadius = (
+  countElementsInArray = 0,
+  elementOrFunction = undefined
+) => {
+  return [...new Array(countElementsInArray)].map(() => elementOrFunction);
+};
+
+console.log(getArrayRandomBorderRadius())
