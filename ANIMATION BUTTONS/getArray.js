3ee0345ff2a892f1221a63c23b4b4d32487ba864
AndriiKot <a_kocyuba@ukr.net>
2023-12-25 14:50:15 +0100

    testing fn getArrayRandomBorderRadius and getArrayRandomBorderRadius2

--- ANIMATION BUTTONS/getArray.js
+++ ANIMATION BUTTONS/getArray.js
@@ -1,15 +1,49 @@
 ﻿"use strict";
 
 const isFunction = (element) => {
-    console.log(typeof element === 'function')
+  return typeof element === "function";
 };
 
 const getArrayRandomBorderRadius = (
-  countElementsInArray = 0,
+  countElementsInArray = 2,
   elementOrFunction = undefined
 ) => {
-  return [...new Array(countElementsInArray)].map(() => elementOrFunction);
+  return [...new Array(countElementsInArray)].map(() => {
+    if (isFunction(elementOrFunction)) {
+      return elementOrFunction();
+    }
+   return elementOrFunction;
+  });
 };
 
-isFunction(function() {})
-isFunction(() => {})
+const getArrayRandomBorderRadius2 = (
+  countElementsInArray = 2,
+  elementOrFunction = undefined
+) => {
+  return [...new Array(countElementsInArray)].map(() => {
+    return (
+      (isFunction(elementOrFunction) && elementOrFunction()) ||
+      elementOrFunction
+    );
+  });
+};
+
+console.log(getArrayRandomBorderRadius());
+console.log(getArrayRandomBorderRadius(3, 5));
+console.log(getArrayRandomBorderRadius(3, null));
+console.log(
+  getArrayRandomBorderRadius(3, () => {
+    return 8;
+  })
+);
+
+console.log('\nVersion 2: \n')
+
+console.log(getArrayRandomBorderRadius2());
+console.log(getArrayRandomBorderRadius2(3, 5));
+console.log(getArrayRandomBorderRadius2(3, null));
+console.log(
+  getArrayRandomBorderRadius2(3, () => {
+    return 8;
+  })
+);
