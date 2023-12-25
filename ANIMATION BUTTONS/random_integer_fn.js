6cb39a53d6e38acf8a7c84180407a6e7e171f311
AndriiKot <a_kocyuba@ukr.net>
2023-12-25 12:49:19 +0100

    create new file 'random_integer_fn.js'

---
+++ ANIMATION BUTTONS/random_integer_fn.js
@@ -0,0 +1,7 @@
+﻿"use strict";
+
+function randomInteger(min = 1, max = 3) {
+  return Math.floor((Math.random() * max) + min);
+}
+
+console.log(randomInteger());
--- ANIMATION BUTTONS/script.js
+++ ANIMATION BUTTONS/script.js
@@ -7,62 +7,65 @@ function randomInteger0_100() {
   return Math.floor(rand);
 }
 
-function myCrazyButton(element, fn = randomInteger0_100) {
-  let el_1 = `${fn()}% ${fn()}% ${fn()}% ${fn()}%`;
-  let el_2 = `${fn()}% ${fn()}% ${fn()}% ${fn()}%`;
-  return (element.style.borderRadius = el_1 + " / " + el_2);
-}
+const testRandom = randomInteger0_100.bind(this,0,10)
+console.log(testRandom)
 
-const getArrayRandomBorderRadius = (
-  countElements = 8,
-  fn = randomInteger0_100
-) => {
-  return [...new Array(countElements)].map(() => fn());
-};
+// function myCrazyButton(element, fn = randomInteger0_100) {
+//   let el_1 = `${fn()}% ${fn()}% ${fn()}% ${fn()}%`;
+//   let el_2 = `${fn()}% ${fn()}% ${fn()}% ${fn()}%`;
+//   return (element.style.borderRadius = el_1 + " / " + el_2);
+// }
 
-const getArrayDefaultBorderRadius = (countElements = 8, element = 0) => {
-  return [...new Array(countElements)].map(() => element);
-};
+// const getArrayRandomBorderRadius = (
+//   countElements = 8,
+//   fn = randomInteger0_100
+// ) => {
+//   return [...new Array(countElements)].map(() => fn());
+// };
 
-async function comparison_of_values(el_1,el_2,button) {
-  button = document.querySelector('button').style.borderRadius;
-  el_2 = getArrayRandomBorderRadius();
-  const test_fun = () => {
-    return [...el_2].map((el) => el += 3)
-  }
-  el_1 = !!button && button || test_fun();              
-  if (typeof el_1 === 'string') {
-    let new_string = el_1.replace(/[%/]/g,'')
-    new_string = new_string.replace('  ',' ')
-    let new_values_array = new_string.split(' ')
-    new_values_array = new_values_array.map((el) => el = +el)
+// const getArrayDefaultBorderRadius = (countElements = 8, element = 0) => {
+//   return [...new Array(countElements)].map(() => element);
+// };
+
+// async function comparison_of_values(el_1,el_2,button) {
+//   button = document.querySelector('button').style.borderRadius;
+//   el_2 = getArrayRandomBorderRadius();
+//   const test_fun = () => {
+//     return [...el_2].map((el) => el += 3)
+//   }
+//   el_1 = !!button && button || test_fun();              
+//   if (typeof el_1 === 'string') {
+//     let new_string = el_1.replace(/[%/]/g,'')
+//     new_string = new_string.replace('  ',' ')
+//     let new_values_array = new_string.split(' ')
+//     new_values_array = new_values_array.map((el) => el = +el)
     
-    el_1 = new_values_array
-  } else {
-  }
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-  async function FF() {
-    let count = 0;
-    while (count < 8) {
-      count = 0;
-      for (let i = 0; i < el_1.length; i++) {
-        if (el_1[i] === el_2[i]) {
-          console.log("Равны!!!")
-          count++;
-          continue;
-        } else {
-          el_1[i] > el_2[i] ? (el_1[i] -= 1) : (el_1[i] += 1);
-        }
-        crazy_button.style.borderRadius = `${el_1[0]}% ${el_1[1]}% ${el_1[2]}% ${el_1[3]}% / ${el_1[4]}% ${el_1[5]}% ${el_1[6]}% ${el_1[7]}%`;
-        await sleep(0);
-      }
-    }
-  }
-  await FF()
-  await comparison_of_values()
-};
+//     el_1 = new_values_array
+//   } else {
+//   }
+//   function sleep(ms) {
+//     return new Promise((resolve) => setTimeout(resolve, ms));
+//   }
+//   async function FF() {
+//     let count = 0;
+//     while (count < 8) {
+//       count = 0;
+//       for (let i = 0; i < el_1.length; i++) {
+//         if (el_1[i] === el_2[i]) {
+//           console.log("Равны!!!")
+//           count++;
+//           continue;
+//         } else {
+//           el_1[i] > el_2[i] ? (el_1[i] -= 1) : (el_1[i] += 1);
+//         }
+//         crazy_button.style.borderRadius = `${el_1[0]}% ${el_1[1]}% ${el_1[2]}% ${el_1[3]}% / ${el_1[4]}% ${el_1[5]}% ${el_1[6]}% ${el_1[7]}%`;
+//         await sleep(0);
+//       }
+//     }
+//   }
+//   await FF()
+//   await comparison_of_values()
+// };
 
 
-crazy_button.addEventListener('click',comparison_of_values)
+// crazy_button.addEventListener('click',comparison_of_values)
