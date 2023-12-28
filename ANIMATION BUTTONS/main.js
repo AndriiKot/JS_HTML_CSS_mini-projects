7f98adc858ee779e947cda25e1cdd2dd816d0fbc
AndriiKot <a_kocyuba@ukr.net>
2023-12-28 10:48:00 +0100

    sleep function

--- ANIMATION BUTTONS/main.js
+++ ANIMATION BUTTONS/main.js
@@ -4,6 +4,8 @@ import randomInteger from "./src/random_integer_fn.js";
 import createArray from "./src/createArray/createArray.js";
 import handleClick from "./src/toggleHandleClick.js";
 import convertStringToArrayInteger from "./src/convertStringToArrayInteger/convertStringToArrayInteger.js";
+import sleep from "./src/sleepAsyncFn.js"
+
 
 const crazy_button = document.querySelector("button");
 const animationGoClassName = "animationGo";
@@ -33,10 +35,6 @@ const convertingValueBorderRadiusToArray = (
 ) => {
   return element ? convertStringToArrayInteger(element) : array;
 };
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
 
 function animationBorderRadius(
   fnConvertToArray = convertingValueBorderRadiusToArray,
