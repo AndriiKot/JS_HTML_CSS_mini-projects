198289a8136a7849a5f3c10c2c04ad746909bae7
AndriiKot <a_kocyuba@ukr.net>
2024-01-02 22:17:42 +0100

    split file functions.js '

---
+++ DROPDOWN/js/dropdown_1.0.2(plus webpack)/src/toggleCheckbox.js
@@ -0,0 +1,9 @@
+﻿
+const toggleCheckbox = (e) => {
+  if (e.target.type === "checkbox") return;
+
+  const checkbox = e.currentTarget.querySelector("input[type='checkbox']");
+  checkbox.checked = !checkbox.checked;
+};
+
+export default toggleCheckbox;
