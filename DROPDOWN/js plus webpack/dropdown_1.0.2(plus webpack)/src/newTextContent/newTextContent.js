68a8eb02a9b31cf04af323cfb7331253a1360159
AndriiKot <a_kocyuba@ukr.net>
2024-01-03 22:36:21 +0100

    create new fn 'createNewValueTextContent'

--- DROPDOWN/js plus webpack/dropdown_1.0.2(plus webpack)/src/newTextContent/newTextContent.js
+++ DROPDOWN/js plus webpack/dropdown_1.0.2(plus webpack)/src/newTextContent/newTextContent.js
@@ -3,17 +3,20 @@ import setArrayBackend from './setArrayBackend';
 import setArrayFrontend from './setArrayFrontend';
 import setArrayFullStack from './setArrayFullStack';
 
-console.log(setArrayFullStack);
 
-const newTextContent = (element, oldValue, newValue, selectorNodeList = '',
-_pointEvent,
+const createNewValueTextContent = (
 setFullStack = setArrayFullStack,
 setBackend = setArrayBackend,
 setFrontend = setArrayFrontend
 ) => {
-  const nodeListItems = document.querySelectorAll(selectorNodeList);
+  console.log(setFullStack,setArrayBackend,setFrontend) //setBackend,setFrontend);
+};
 
-  console.log(setFullStack) //setBackend,setFrontend);
+createNewValueTextContent();
+
+const newTextContent = (element, oldValue, newValue, selectorNodeList = '',
+) => {
+  const nodeListItems = document.querySelectorAll(selectorNodeList);
 
   let count = 0;
   nodeListItems.forEach((item) => {
