e975236d3c63ec2df5603b57cc486f77354c97b7
AndriiKot <a_kocyuba@ukr.net>
2024-01-03 20:40:40 +0100

    test

--- DROPDOWN/js plus webpack/dropdown_1.0.2(plus webpack)/src/checkSubset.js
+++ DROPDOWN/js plus webpack/dropdown_1.0.2(plus webpack)/src/checkSubset.js
@@ -1,7 +1,7 @@
 ﻿
 const checkSubset = (parentArray = [], subsetArray = []) => {
   console.log("HI!!")
-  return subsetArray.every((el) => parentArray.includes(el));
+  // return subsetArray.every((el) => parentArray.includes(el));
 };
 
 export default checkSubset;
--- DROPDOWN/js plus webpack/dropdown_1.0.2(plus webpack)/src/newTextContent.js
+++ DROPDOWN/js plus webpack/dropdown_1.0.2(plus webpack)/src/newTextContent.js
@@ -1,15 +1,4 @@
-﻿import arrayBackend from './arrayBackend';
-import arrayFrontend from './arrayFrontend';
-import arrayFullStack from './arrayFullStack';
-// import checkSubset from './checkSubset';
-
-// console.log(checkSubset());
-console.log(arrayBackend);
-console.log(arrayFrontend);
-console.log(arrayFullStack);
-console.log('TEST fn ChekSubset: ');
-// console.log(checkSubset(arrayFullStack, arrayFrontend));
-
+﻿
 const newTextContent = (element, oldValue, newValue, selectorNodeList = '') => {
   const nodeListItems = document.querySelectorAll(selectorNodeList);
   let count = 0;
