0a1526b32cd8ec079ff8fb233799574f2b7fbb29
AndriiKot <a_kocyuba@ukr.net>
2024-01-04 20:08:34 +0100

    create dropdown webpack 1_0_

---
+++ DROPDOWN/js plus webpack/dropdown_1.0.1(plus webpack)/src/functions.js
@@ -0,0 +1,21 @@
+﻿
+
+
+const toggleCheckbox = (e) => {
+  if (e.target.type === 'checkbox') return;
+
+  const checkbox = e.currentTarget.querySelector('input[type=\'checkbox\']');
+  checkbox.checked = !checkbox.checked;
+};
+
+const newTextContent = (element, oldValue, newValue, selectorNodeList = '') => {
+  const nodeListItems = document.querySelectorAll(selectorNodeList);
+  let count = 0;
+  nodeListItems.forEach((item) => {
+    if (item.checked) count++;
+  });
+  element.textContent = count ? newValue + String(count) : oldValue;
+};
+
+
+export { toggleCheckbox, newTextContent };
