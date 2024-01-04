cc9aa4e4630c339f9e537fe729bdba21e1670fee
AndriiKot <a_kocyuba@ukr.net>
2024-01-04 20:44:09 +0100

    replacing bind with an arrow function

--- DROPDOWN/js plus webpack/dropdown_1.0.1(plus webpack)/app.js
+++ DROPDOWN/js plus webpack/dropdown_1.0.1(plus webpack)/app.js
@@ -1,41 +1,35 @@
-﻿
-import { handleClick } from './src/handleClick.js';
-import { toggleCheckbox } from './src/toggleCheckbox.js';
-import { newTextContent } from './src/newTextContent.js';
+﻿import { handleClick } from "./src/handleClick.js";
+import { toggleCheckbox } from "./src/toggleCheckbox.js";
+import { newTextContent } from "./src/newTextContent.js";
 
-const selectButton = document.querySelector('.select-btn');
+const selectButton = document.querySelector(".select-btn");
 const defaultButtonTextContent =
-  selectButton.querySelector('.text-content').innerText;
-const newButtonTextContent = 'Selected ';
-const selectButtonActiveClassName = 'active';
+  selectButton.querySelector(".text-content").innerText;
+const newButtonTextContent = "Selected ";
+const selectButtonActiveClassName = "active";
 
-const dropDown = document.querySelector('.list-items');
-const liNodeList = dropDown.querySelectorAll('.item');
-const checkboxClassName = '.list-items__item__checkbox';
-const dropDownExistsClassName = 'exists';
+const dropDown = document.querySelector(".list-items");
+const liNodeList = dropDown.querySelectorAll(".item");
+const checkboxClassName = ".list-items__item__checkbox";
+const dropDownExistsClassName = "exists";
+const selectButtonTextContent = document.querySelector(".text-content");
 
-const selectButtonTextContent = document.querySelector('.text-content');
+selectButton.addEventListener("click", () => {
+  handleClick(selectButton, selectButtonActiveClassName);
+});
 
-selectButton.addEventListener(
-  'click',
-  handleClick.bind(this, selectButton, selectButtonActiveClassName)
-);
-
-selectButton.addEventListener(
-  'click',
-  handleClick.bind(this, dropDown, dropDownExistsClassName)
-);
+selectButton.addEventListener("click", () => {
+  handleClick(dropDown, dropDownExistsClassName);
+});
 
 liNodeList.forEach((item) => {
-  item.addEventListener('click', toggleCheckbox);
-  item.addEventListener(
-    'click',
-    newTextContent.bind(
-      this,
+  item.addEventListener("click", toggleCheckbox);
+  item.addEventListener("click", () => {
+    newTextContent(
       selectButtonTextContent,
       defaultButtonTextContent,
       newButtonTextContent,
       checkboxClassName
-    )
-  );
+    );
+  });
 });
