8b8572dfc9e42573a2b3869b687cfd7bed093274
AndriiKot <a_kocyuba@ukr.net>
2024-01-06 11:14:20 +0100

    toggle class='modal-show'

--- MODAL WINDOW/modal window v1/modal window(JS CSS HTML)/script.js
+++ MODAL WINDOW/modal window v1/modal window(JS CSS HTML)/script.js
@@ -3,9 +3,8 @@ const buttonModalWindow = document.querySelector('.btn-show__modal_widow');
 const modalWindow = document.querySelector('.modal')
 
 const showModalWindow = (_event,element = modalWindow) => {
-    console.log('Hi! I am ModalWindow!')
-    console.log(modalWindow)
-    console.log(element)
+    element.classList.toggle('modal-show')
 };
 
 buttonModalWindow.addEventListener('click', showModalWindow)
+
