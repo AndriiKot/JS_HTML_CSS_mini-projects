68794c8b25aeec4b2a71d88efc71bf5447915336
AndriiKot <a_kocyuba@ukr.net>
2024-01-06 10:40:58 +0100

    create fn 'showModalWindow'

--- MODAL WINDOW/modal window v1/modal window(JS CSS HTML)/script.js
+++ MODAL WINDOW/modal window v1/modal window(JS CSS HTML)/script.js
@@ -1,8 +1,11 @@
 ﻿
 const buttonModalWindow = document.querySelector('.btn-show__modal_widow');
+const modalWindow = document.querySelector('.modal')
 
-const showModalWindow = () => {
+const showModalWindow = (_event,element = modalWindow) => {
     console.log('Hi! I am ModalWindow!')
+    console.log(modalWindow)
+    console.log(element)
 };
 
 buttonModalWindow.addEventListener('click', showModalWindow)
