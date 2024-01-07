7799a3231aa0345255d4aa020f62a5c188afc35b
AndriiKot <a_kocyuba@ukr.net>
2024-01-07 23:30:17 +0100

    npm install grunt-contrib-less --save-dev

---
+++ MODAL WINDOW/modal window v1/modal window(Less JS CSS HTML)/Gruntfile.js
@@ -0,0 +1,41 @@
+﻿module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-exec');
+
+    grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+        files: ['**/*.ts'],
+        tasks: ['exec:run_tsc']
+    },
+    exec: {
+        run_tsc: {cmd: 'tsc'}
+    }
+    });
+
+    grunt.registerTask('default', ['watch']);
+    
+};
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["./assets/stylesheets/less"],
+                    yuicompress: true
+                },
+                files: {
+                    "./assets/stylesheets/css/style.css": "./assets/stylesheets/less/style.less"
+                }
+            }
+        },
+        watch: {
+            files: "./assets/stylesheets/less/*",
+            tasks: ["less"]
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
