447ac140e1b670844fb44ab3483ce0e5b373c630
AndriiKot <a_kocyuba@ukr.net>
2024-01-08 10:49:11 +0100

    create initConfig-Grunt

--- MODAL WINDOW/modal window v1/modal window(Less JS CSS HTML)/Gruntfile.js
+++ MODAL WINDOW/modal window v1/modal window(Less JS CSS HTML)/Gruntfile.js
@@ -1,4 +1,5 @@
 ﻿module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-less");
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.initConfig({ pkg: grunt.file.readJSON("package.json") });
 };
