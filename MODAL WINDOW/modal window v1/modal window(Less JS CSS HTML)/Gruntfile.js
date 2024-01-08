9d06072ab55f953bb8350eb3f9f4bbb81385ab07
AndriiKot <a_kocyuba@ukr.net>
2024-01-08 11:23:59 +0100

    test Gruntfile

--- MODAL WINDOW/modal window v1/modal window(Less JS CSS HTML)/Gruntfile.js
+++ MODAL WINDOW/modal window v1/modal window(Less JS CSS HTML)/Gruntfile.js
@@ -1,5 +1,30 @@
 ﻿module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-less");
   grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.initConfig({ pkg: grunt.file.readJSON("package.json") });
+  // grunt.initConfig({
+  //   // pkg: grunt.file.readJSON("package.json"),
+  //   less: {
+  //     options: {
+  //       paths: "./css/less/",
+  //       yuicompress: true,
+  //     },
+  //     files: {
+  //       "style.css": "css/less/style.less",
+  //     },
+  //   },
+  // });
+  grunt.initConfig({
+    less: {
+       development: {
+          options: {
+             compress: true,
+             yuicompress: true,
+             optimization: 2
+          },
+          files: {
+             // target.css file: source.less file
+             "css/main.css": "less/main.less"
+          }
+      }
+ },
 };
