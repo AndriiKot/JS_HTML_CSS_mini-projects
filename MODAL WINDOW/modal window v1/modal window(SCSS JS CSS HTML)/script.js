a50e7c999e8ea20e25f171df35540e5a753d4b80
AndriiKot <a_kocyuba@ukr.net>
2024-01-09 22:52:21 +0100

    version 0.0.1 SCSS ready

--- MODAL WINDOW/modal window v1/modal window(SCSS JS CSS HTML)/script.js
+++ MODAL WINDOW/modal window v1/modal window(SCSS JS CSS HTML)/script.js
@@ -1,23 +1,11 @@
-﻿
+﻿"use strict";
 const buttonModalWindow = document.querySelector('.btn-show__modal_widow');
 const buttonCloseModalWindow = document.querySelector('.modal-window__btn-close');
 const modalWindow = document.querySelector('.modal')
 
-
-const showModalWindow = (element,method) => {
-    element.classList[method]('modal-show')
-};
-
-const hiddenModalWindow = (element,method) => {
-    element.classList[method]('modal-show')
-};
-
 const addOrRemoveClass = (element,method,className) => {
     element.classList[method](className)
 }
-
-// buttonModalWindow.addEventListener('click', () => {showModalWindow(modalWindow,'add')});
-// buttonCloseModalWindow.addEventListener('click', () => {hiddenModalWindow(modalWindow,'remove')});
 
 buttonModalWindow.addEventListener('click', () => {
     const showModalWindow = addOrRemoveClass(modalWindow,'add','modal-show')
@@ -27,6 +15,4 @@ buttonCloseModalWindow.addEventListener('click', () => {
     const hiddenModalWindow = addOrRemoveClass(modalWindow,'remove','modal-show')
 });
 
-// const newFunction = addOrRemoveClass(modalWindow,'add','modal-show');
-// console.dir({newFunction})
 
