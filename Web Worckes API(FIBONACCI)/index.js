3b21a77073964ee5dbfa65583ba79d1c77d02cd3
AndriiKot <a_kocyuba@ukr.net>
2024-01-12 21:09:59 +0100

    testing input submit class Name

--- Web Worckes API(FIBONACCI)/index.js
+++ Web Worckes API(FIBONACCI)/index.js
@@ -1,26 +1,24 @@
 ﻿'use strict';
 
 const form_fibonacci = document.querySelector('.text-number__fibonacci-wrapper');
-const input_fibonacci = document.querySelector('.text-number__fibonacci-input');
+const input_number_fibonacci = document.querySelector('.text-number__fibonacci-input');
+const input_submit_fibonacci = document.querySelector('.wrapper__fibonacci__input-submit')
 
 form_fibonacci.addEventListener('mouseover', (e) => { 
     e.target.focus()
 })
 
-input_fibonacci.addEventListener('focus', (e) => {
-    console.log('Focus!!!')
-    console.log(e.keyCode)
-})
+console.log(input_submit_fibonacci)
 
-input_fibonacci.addEventListener('Enter', (e) => {
-    console.log("Enter!!")
-})
+// input_number_fibonacci.addEventListener('focus', (e) => {
+//     console.log('Focus!!!')
+//     document.addEventListener('keypress' (e) => {
+//         if (e.key === "Enter") {
+//             e.preventDefault();
+//             document.getElementById("myBtn").click();
+//           }
+      
+//     })
+// })
 
-// document.onkeyup = function (e) {
-//             e = e || window.event;
-//             if (e.keyCode === 13) {
-//                 searchtov();
-//             }
-//             // Отменяем действие браузера
-//             return false;
-// }
+
