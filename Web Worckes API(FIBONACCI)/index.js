dcafdba995376dbc7cd8c568c41115e8cc27f455
AndriiKot <a_kocyuba@ukr.net>
2024-01-12 16:26:32 +0100

    test hover event

--- Web Worckes API(FIBONACCI)/index.js
+++ Web Worckes API(FIBONACCI)/index.js
@@ -1,4 +1,5 @@
 ﻿'use strict';
 
 const input_fibonacci = document.querySelector('.text-number__fibonacci-wrapper');
+input_fibonacci.addEventListener('mouseover', () => { console.log('event hover') })
 console.log(input_fibonacci);
