c51be3daf20325d791f867e382cca00a8e6795b9
AndriiKot <a_kocyuba@ukr.net>
2024-01-12 20:55:44 +0100

    testing event 'focus'

--- Web Worckes API(FIBONACCI)/index.js
+++ Web Worckes API(FIBONACCI)/index.js
@@ -1,6 +1,21 @@
 ﻿'use strict';
 
-const input_fibonacci = document.querySelector('.text-number__fibonacci-wrapper');
-input_fibonacci.addEventListener('mouseover', (e) => { 
+const form_fibonacci = document.querySelector('.text-number__fibonacci-wrapper');
+const input_fibonacci = document.querySelector('.text-number__fibonacci-input');
+
+form_fibonacci.addEventListener('mouseover', (e) => { 
     e.target.focus()
 })
+
+input_fibonacci.addEventListener('focus', (e) => {
+    console.log('Focus!!!')
+})
+
+// document.onkeyup = function (e) {
+//             e = e || window.event;
+//             if (e.keyCode === 13) {
+//                 searchtov();
+//             }
+//             // Отменяем действие браузера
+//             return false;
+// }
