df6eb8cd48cb72bd77e7c0ffe980203a711eba5a
AndriiKot <a_kocyuba@ukr.net>
2024-01-14 18:25:22 +0100

    event click

--- Web Worckes API(FIBONACCI)/index.js
+++ Web Worckes API(FIBONACCI)/index.js
@@ -5,24 +5,17 @@ const form_fibonacci_placeholder_text = document.querySelector('.text-number__fi
 const input_number_fibonacci = document.querySelector('.text-number__fibonacci-input');
 const input_submit_fibonacci = document.querySelector('.wrapper__fibonacci__input-submit');
 
-console.log(form_fibonacci_placeholder_text);
-
 form_fibonacci.addEventListener('mouseover', (e) => { 
     e.target.focus()
     form_fibonacci_placeholder_text.style.opacity = '0.8';
 })
-
 
 input_number_fibonacci.addEventListener('focus', (e) => {
     document.addEventListener('keypress', (e) => {
         if (!e.target.value) { return }
         if (e.key === "Enter" ) {
             document.querySelector('.wrapper__fibonacci__input-submit').click();
-            e.target.type = 'text'
-            console.log('Click')
-            console.dir(e.target.type)
-            e.target.value = 'Идёт вычисление!!'
-
+            e.target.value = '';
           }
       
     })
