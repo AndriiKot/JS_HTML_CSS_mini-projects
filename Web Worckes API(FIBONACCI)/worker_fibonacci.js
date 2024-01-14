7cb23def483e9076db76b5f366055feebade111b
AndriiKot <a_kocyuba@ukr.net>
2024-01-14 20:10:50 +0100

    test fn fibonacci

--- Web Worckes API(FIBONACCI)/worker_fibonacci.js
+++ Web Worckes API(FIBONACCI)/worker_fibonacci.js
@@ -1,13 +1,13 @@
 ﻿
 const fibonacci = (n) => {
-    n = +n;
-    if(n < 2) return n;
+    // console.log(typeof(n));
+    n = +n
+    if(n < 2) return n
     return fibonacci(n - 1) + fibonacci(n  - 2)
 }
 
 this.addEventListener('message', (message) => {
-    console.log(`Message from worker:  ${message}!`)
-    console.dir({ message });
     const { data } = message;
     console.log(data)
+    this.postMessage(fibonacci(data));
 })
