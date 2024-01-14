e6ba867ca6f0654f8e5261f5c8dd3c0b7a343df6
AndriiKot <a_kocyuba@ukr.net>
2024-01-15 00:45:28 +0100

    testing worker resalt

--- Web Worckes API(FIBONACCI)/index.js
+++ Web Worckes API(FIBONACCI)/index.js
@@ -1,36 +1,44 @@
-﻿'use strict';
+﻿"use strict";
 
-const form_fibonacci = document.querySelector('.text-number__fibonacci-wrapper');
-const form_fibonacci_placeholder_text = document.querySelector('.text-number__fibonacci-placeholder');
-const input_number_fibonacci = document.querySelector('.text-number__fibonacci-input');
-const input_submit_fibonacci = document.querySelector('.wrapper__fibonacci__input-submit');
+const form_fibonacci = document.querySelector(
+  ".text-number__fibonacci-wrapper"
+);
+const form_fibonacci_placeholder_text = document.querySelector(
+  ".text-number__fibonacci-placeholder"
+);
+const input_number_fibonacci = document.querySelector(
+  ".text-number__fibonacci-input"
+);
+const input_submit_fibonacci = document.querySelector(
+  ".wrapper__fibonacci__input-submit"
+);
 
-form_fibonacci.addEventListener('mouseover', (e) => { 
-    e.target.focus()
-    form_fibonacci_placeholder_text.style.opacity = '0.8';
-})
+form_fibonacci.addEventListener("mouseover", (e) => {
+  e.target.focus();
+  form_fibonacci_placeholder_text.style.opacity = "0.8";
+});
 
-input_number_fibonacci.addEventListener('focus', (e) => {
-    document.addEventListener('keypress', (e) => {
-        if (!e.target.value) { return }
-        if (e.key === "Enter" ) {
-            document.querySelector('.wrapper__fibonacci__input-submit').click();
-            e.target.setAttribute('type', 'text');
-            e.target.value = 'Resalt: ';
-            console.dir(e.target.value);
-          }   
-    })
-})
-
-if(window.Worker) {
+const worker = (argMessage) => {
+  if (window.Worker) {
     const worker_fibonacci = new Worker("./worker_fibonacci.js");
-    worker_fibonacci.postMessage('5');
+    worker_fibonacci.postMessage(argMessage);
 
     worker_fibonacci.onmessage = (message) => {
-        console.log(message.data)
+      console.log(message.data);
+    };
+  }
+};
+
+input_number_fibonacci.addEventListener("focus", (e) => {
+  document.addEventListener("keypress", (e) => {
+    if (!e.target.value) {
+      return;
     }
-}
-
-
-
-
+    const number = e.target.value;
+    if (e.key === "Enter") {
+      document.querySelector(".wrapper__fibonacci__input-submit").click();
+      e.target.setAttribute("type", "text");
+      e.target.value = "Resalt: " + worker(number);
+    }
+  });
+});
