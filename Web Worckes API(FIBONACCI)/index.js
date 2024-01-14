c1c3592f460b3874f9eab8a20366ea8f3e8f9e6d
AndriiKot <a_kocyuba@ukr.net>
2024-01-14 12:38:53 +0100

    test placeholder-text-style

--- Web Worckes API(FIBONACCI)/index.js
+++ Web Worckes API(FIBONACCI)/index.js
@@ -1,8 +1,11 @@
 ﻿'use strict';
 
 const form_fibonacci = document.querySelector('.text-number__fibonacci-wrapper');
+const form_fibonacci_placeholder_text = document.querySelector('.text-number__fibonacci-placeholder');
 const input_number_fibonacci = document.querySelector('.text-number__fibonacci-input');
-const input_submit_fibonacci = document.querySelector('.wrapper__fibonacci__input-submit')
+const input_submit_fibonacci = document.querySelector('.wrapper__fibonacci__input-submit');
+
+console.log(form_fibonacci_placeholder_text);
 
 form_fibonacci.addEventListener('mouseover', (e) => { 
     e.target.focus()
@@ -18,7 +21,7 @@ input_number_fibonacci.addEventListener('focus', (e) => {
             console.log('Click')
             console.dir(e.target.type)
             e.target.value = 'Идёт вычисление!!'
-            
+
           }
       
     })
