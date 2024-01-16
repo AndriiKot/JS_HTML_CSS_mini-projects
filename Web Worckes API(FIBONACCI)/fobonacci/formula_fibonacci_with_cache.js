c8de39cc8fbfd54ae61adb674dc37260fd7771f9
AndriiKot <a_kocyuba@ukr.net>
2024-01-16 10:47:52 +0100

    split fibonacci

--- Web Worckes API(FIBONACCI)/fobonacci/formula_fibonacci_with_cache.js
+++ Web Worckes API(FIBONACCI)/fobonacci/formula_fibonacci_with_cache.js
@@ -1,1 +1,16 @@
-﻿
+﻿const old_fibonacci = (n) => {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+};
+
+const memoize = (fn_fibonacci) => {
+  const cache = {};
+  return (arg) => cache[arg] || (cache[arg] = fn_fibonacci(arg));
+};
+
+const fibonacci = memoize(old_fibonacci);
+
+module.exports = fibonacci;
--- Web Worckes API(FIBONACCI)/fobonacci/valid_result_fibonacci.js
+++ Web Worckes API(FIBONACCI)/fobonacci/valid_result_fibonacci.js
@@ -1,18 +1,4 @@
-﻿
-const old_fibonacci = (n) => {
-  if (n <= 1) {
-    return n;
-  } else {
-    return fibonacci(n - 1) + fibonacci(n - 2);
-  }
-};
-
-const memoize = (fn_fibonacci) => {
-  const cache = {};
-  return (arg) => cache[arg] || (cache[arg] = fn_fibonacci(arg));
-};
-
-const fibonacci = memoize(old_fibonacci);
+﻿const fibonacci = require('./formula_fibonacci_with_cache');
 
 const max_integer_fibonacci = (fn) => {
   let i = 1000;
@@ -25,3 +11,4 @@ const max_integer_fibonacci = (fn) => {
   }
 };
 
+console.log(max_integer_fibonacci(fibonacci));
