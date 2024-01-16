9d81c7ed34ac6cc1880d965ae8d400978601c14e
AndriiKot <a_kocyuba@ukr.net>
2024-01-16 16:16:15 +0100

    fibonacci v 0.0.1

--- Web Worckes API(FIBONACCI)/worker_fibonacci.js
+++ Web Worckes API(FIBONACCI)/worker_fibonacci.js
@@ -1,18 +1,18 @@
 ﻿
-// const old_fibonacci = (n) => {
-//   if (n <= 1) {
-//     return n;
-//   } else {
-//     return fibonacci(n - 1) + fibonacci(n - 2);
-//   }
-// };
+const old_fibonacci = (n) => {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+};
 
-// const memoize = (fn_fibonacci) => {
-//   const cache = {};
-//   return (arg) => cache[arg] || (cache[arg] = fn_fibonacci(arg));
-// };
+const memoize = (fn_fibonacci) => {
+  const cache = {};
+  return (arg) => cache[arg] || (cache[arg] = fn_fibonacci(arg));
+};
 
-// const fibonacci = memoize(old_fibonacci);
+const fibonacci = memoize(old_fibonacci);
 
 this.addEventListener("message", (message) => {
   const { data } = message;
