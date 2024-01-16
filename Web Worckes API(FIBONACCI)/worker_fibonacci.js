23d2b3ebd4e7bc6dc154e4e27794edc010501c41
AndriiKot <a_kocyuba@ukr.net>
2024-01-16 09:25:05 +0100

    fibonacci fn cache

--- Web Worckes API(FIBONACCI)/worker_fibonacci.js
+++ Web Worckes API(FIBONACCI)/worker_fibonacci.js
@@ -1,11 +1,19 @@
-﻿
-const fibonacci = (n) => {
-    n = +n
-    if(n < 2) return n
-    return fibonacci(n - 1) + fibonacci(n  - 2)
-}
+﻿const old_fibonacci = (n) => {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+};
 
-this.addEventListener('message', (message) => {
-    const { data } = message;
-    this.postMessage(fibonacci(data));
-})
+const memoize = (fn_fibonacci) => {
+  const cache = {};
+  return (arg) => cache[arg] || (cache[arg] = fn_fibonacci(arg));
+};
+
+const fibonacci = memoize(old_fibonacci);
+
+this.addEventListener("message", (message) => {
+  const { data } = message;
+  this.postMessage(old_fibonacci(data));
+});
