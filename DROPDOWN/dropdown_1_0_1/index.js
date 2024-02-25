8645c62cf5ff3a501c5cea593d6eb98a04941ef1
AndriiKot <a_kocyuba@ukr.net>
2024-02-25 13:52:50 +0100

    test css 'active-test'

--- DROPDOWN/dropdown_1_0_1/index.js
+++ DROPDOWN/dropdown_1_0_1/index.js
@@ -1,62 +1,62 @@
-﻿const dropDownMainWrapper = document.querySelector(".wrapper");
+const dropDownMainWrapper = document.querySelector(".wrapper");
 const selectButton = dropDownMainWrapper.querySelector(".select-btn");
 
 dropDownMainWrapper.addEventListener("click", (e) => {
-  e.currentTarget.classList.toggle("TEST-ACTIVE");
+  e.currentTarget.classList.toggle("active_test");
 });
 
-const defaultButtonTextContent =
-  selectButton.querySelector(".text-content").innerText;
-const newButtonTextContent = "Selected ";
-const selectButtonActiveClassName = "active";
+// const defaultButtonTextContent =
+//   selectButton.querySelector(".text-content").innerText;
+// const newButtonTextContent = "Selected ";
+// const selectButtonActiveClassName = "active";
 
-const dropDown = dropDownMainWrapper.querySelector(".list-items");
-const liNodeList = dropDown.querySelectorAll(".item");
-const dropDownExistsClassName = "exists";
+// const dropDown = dropDownMainWrapper.querySelector(".list-items");
+// const liNodeList = dropDown.querySelectorAll(".item");
+// const dropDownExistsClassName = "exists";
 
-const handleSelectButtonClick = (element, className) => {
-  element.classList.toggle(className);
-};
+// const handleSelectButtonClick = (element, className) => {
+//   element.classList.toggle(className);
+// };
 
-selectButton.addEventListener(
-  "click",
-  handleSelectButtonClick.bind(this, selectButton, selectButtonActiveClassName)
-);
+// selectButton.addEventListener(
+//   "click",
+//   handleSelectButtonClick.bind(this, selectButton, selectButtonActiveClassName)
+// );
 
-selectButton.addEventListener(
-  "click",
-  handleSelectButtonClick.bind(this, dropDown, dropDownExistsClassName)
-);
+// selectButton.addEventListener(
+//   "click",
+//   handleSelectButtonClick.bind(this, dropDown, dropDownExistsClassName)
+// );
 
-const toggleCheckbox = (e) => {
-  if (e.target.type === "checkbox") return;
+// const toggleCheckbox = (e) => {
+//   if (e.target.type === "checkbox") return;
 
-  const checkbox = e.currentTarget.querySelector("input[type='checkbox']");
-  checkbox.checked = checkbox.checked ? false : true;
-};
+//   const checkbox = e.currentTarget.querySelector("input[type='checkbox']");
+//   checkbox.checked = checkbox.checked ? false : true;
+// };
 
-const newTextContentSelectButton = (element, oldValue, newValue) => {
-  const nodeListItems = document.querySelectorAll(
-    ".list-items__item__checkbox"
-  );
-  let count = 0;
-  nodeListItems.forEach((item) => {
-    if (item.checked) count++;
-  });
-  element.textContent = count ? newValue + String(count) : oldValue;
-};
+// const newTextContentSelectButton = (element, oldValue, newValue) => {
+//   const nodeListItems = document.querySelectorAll(
+//     ".list-items__item__checkbox"
+//   );
+//   let count = 0;
+//   nodeListItems.forEach((item) => {
+//     if (item.checked) count++;
+//   });
+//   element.textContent = count ? newValue + String(count) : oldValue;
+// };
 
-const selectButtonTextContent = document.querySelector(".text-content");
+// const selectButtonTextContent = document.querySelector(".text-content");
 
-liNodeList.forEach((item) => {
-  item.addEventListener("click", toggleCheckbox);
-  item.addEventListener(
-    "click",
-    newTextContentSelectButton.bind(
-      this,
-      selectButtonTextContent,
-      defaultButtonTextContent,
-      newButtonTextContent
-    )
-  );
-});
+// liNodeList.forEach((item) => {
+//   item.addEventListener("click", toggleCheckbox);
+//   item.addEventListener(
+//     "click",
+//     newTextContentSelectButton.bind(
+//       this,
+//       selectButtonTextContent,
+//       defaultButtonTextContent,
+//       newButtonTextContent
+//     )
+//   );
+// });
